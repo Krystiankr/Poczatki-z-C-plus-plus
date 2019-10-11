0a013108038829fa04ed354e1c05b8d0a4320ef4
Krystiankr <56454269+Krystiankr@users.noreply.github.com>
2019-10-11 21:50:37 +0200

    Update Choinka.cpp

--- Choinka.cpp
+++ Choinka.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 using namespace std;
 int main() {
-	int a = 3;//b/;
-	//cin >> b;
-	for (int b=3; b > 0; --b) {
-		for (int i = 0; i < a; ++i) {
-			for (int j = 0; j <= a - 2 - i+b-1; ++j) {
+	int ilosc = 3;
+	int powtarzalnosc = 1;
+	for (; powtarzalnosc > 0; --powtarzalnosc) {
+		for (int i = 0; i < ilosc; ++i) {
+			for (int j = 0; j <= ilosc - 3 - i + powtarzalnosc ; ++j) {
 				cout << " ";
 			}
 			for (int j = 0; j <= 0 + 2 * i; ++j) {
 				cout << "*";
 			}cout << endl;
-		}a += 1;
+		}ilosc += 1;
 	}
 }
