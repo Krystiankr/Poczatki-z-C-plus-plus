12274232601c61cc406331199c2ac548abf31cd5
Krystiankr <56454269+Krystiankr@users.noreply.github.com>
2019-10-20 21:37:36 +0200

    Create Ramka.cpp

---
+++ Ramka.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a, b;
+	cin >> a >> b;
+	for (int i = 0; i < a; i++) {
+		for (int k = 0; k < b; k++) {
+			if (i == 0 or i == 1 or i == a-1 or i == a-2 or k==0 or k==b-1) {
+				if ((i + k) % 2 == 0) {
+					cout << "x";
+				}
+				else { cout << "."; }
+			}
+			else {cout << "."; }
+		}cout << endl;
+	}
+}
