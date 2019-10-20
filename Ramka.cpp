8a92328552183d23bc7c3a5ecfa5adadeeecff8f
Krystiankr <56454269+Krystiankr@users.noreply.github.com>
2019-10-20 21:39:26 +0200

    Update Ramka.cpp

--- Ramka.cpp
+++ Ramka.cpp
@@ -5,7 +5,7 @@ int main() {
 	cin >> a >> b;
 	for (int i = 0; i < a; i++) {
 		for (int k = 0; k < b; k++) {
-			if (i == 0 or i == 1 or i == a-1 or i == a-2 or k==0 or k==b-1) {
+			if (i == 0 or i == 1 or i == a-1 or i == a-2 or k==0 or k==b-1 or k==1 or k==b-2) {
 				if ((i + k) % 2 == 0) {
 					cout << "x";
 				}
