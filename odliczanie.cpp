ad08fa953cd1120ed4dea8e54ebdbe18e23ab84d
Krystiankr <56454269+Krystiankr@users.noreply.github.com>
2019-10-24 23:33:21 +0200

    Create odliczanie.cpp

---
+++ odliczanie.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a, b;
+	cin >> a >> b;
+	if(a<=b)
+		while (a <= b) {
+			cout << a++<<" ";
+		}
+	else
+		while (a >= b) {
+			cout << a--<<" ";
+		}
+}
