2d5241c58d503d723755e378543b3b0b4216eeab
Krystiankr <56454269+Krystiankr@users.noreply.github.com>
2019-11-06 18:43:35 +0100

    Create Wypisywanie liczb bez maksimów.cpp

---
+++ Wypisywanie liczb bez maksimów.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int wejscie;
+	cin >> wejscie;
+	int start = 0;
+	int to=-1;
+	int zmiana=0;
+	int *array = new int[wejscie];
+	for (int i = 0; i < wejscie; i++) {
+		cin >> array[i];
+	}
+	for (int j = 0; j < wejscie; j++) {
+		for (int i = 0; i < wejscie; i++) {
+			if (array[i] > start and array[i] >= 0) {
+				start = array[i];
+				to = i;
+			}
+		}
+			for (int i = 0; i < wejscie; i++) {
+				if (array[i] < start and array[i] >= 0 and zmiana!=start) {
+					cout <<array[i]<<" ";
+				}
+			}if (zmiana != start) { cout << endl; } if (start == 0)break; zmiana = start; start = 0; if (to != -1) { array[to] = -1; }
+		
+
+	}delete[] array;
+}
