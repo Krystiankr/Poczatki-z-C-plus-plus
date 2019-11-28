7a42dd5340da2d66ec8d56b8b02b1b513156d788
Krystiankr <56454269+Krystiankr@users.noreply.github.com>
2019-11-28 02:59:00 +0100

    Update 40.Silnia_1.cpp

--- 40.Silnia_1.cpp
+++ 40.Silnia_1.cpp
@@ -3,12 +3,17 @@
 // stos nie działa mi tak
 using namespace std;
 int main() {
-	int x=1;
+	int x = 1;
 	while (cin >> x, !cin.fail()) {
 		unsigned long long b = 1;
-		for (int i = 1; i <= x; i++) {
-			b *= i;
+		if (x <= 1) {
+			cout << "1\n";
 		}
-		cout << b<<endl;
+		else {
+			for (int i = 1; i <= x; i++) {
+				b *= i;
+			}
+			cout << b << endl;
+		}
 	}
 }
